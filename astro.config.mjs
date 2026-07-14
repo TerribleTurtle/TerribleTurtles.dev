fe8336647f7b91f6e8d0083206feb7bda3dca88b
TerribleTurtles <5100566+TerribleTurtle@users.noreply.github.com>
2026-07-13 20:40:35 -0400

    chore(infra): enforce static output, add wrangler, and strict color linting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   image: {
     service: passthroughImageService()
   }
