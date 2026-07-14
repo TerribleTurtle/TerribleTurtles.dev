211ce4063e0f8b2fabad228c61f7ceed3c30d273
TerribleTurtles <5100566+TerribleTurtle@users.noreply.github.com>
2026-07-13 23:22:36 -0400

    fix: allow data URIs in font-src for inline woff2 fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  build: {
+    inlineStylesheets: 'always'
+  },
   image: {
     service: passthroughImageService()
   }
